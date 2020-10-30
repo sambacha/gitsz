83c5f7b368c5fe0b84884dce8f005e44ed13f7a4
sam <sam@freighttrust.com>
2020-10-29 17:28:46 -0700

    fix(lib): rename paths
    incorrectly had lib/gitsz as lib/gitz
    
    BREAKING CHANGE: fixes unresolvable module
    
    unable to install

---
+++ test/test_cannonical.bash
@@ -0,0 +1,2 @@
+#!/bin/bash
+GIT_AUTHOR_DATE="Thu, 01 Jan 1970 00:00:00 +0000" GIT_COMMITTER_DATE="Thu, 01 Jan 1970 00:00:00 +0000" git commit --allow-empty --allow-empty-message -m ''
---
+++ test/test_cannonical_tag.bash
@@ -0,0 +1,4 @@
+#!/bin/bash
+export GIT_AUTHOR_DATE="Thu, 01 Jan 1970 00:00:00 +0000" 
+export GIT_COMMITTER_DATE="Thu, 01 Jan 1970 00:00:00 +0000"
+gitsz --allow-empty --allow-empty-message -m ''
