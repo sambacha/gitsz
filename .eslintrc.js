5a9cccf1579ebcbf3413dec7befd010a3c522664
sam <sam@freighttrust.com>
2020-11-04 03:01:15 -0800

    release 1.0.0

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,15 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es6": true,
-        "node": true
-    },
-    "extends": "eslint:recommended",
-    "rules": {
-        "indent": [
-            "error",
-            2
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
-    }
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true,
+  },
+  extends: "eslint:recommended",
+  rules: {
+    indent: ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "single"],
+    semi: ["error", "always"],
+  },
 };
--- test/ev-tag-test.js
+++ test/ev-tag-test.js
@@ -1,18 +1,18 @@
-'use strict';
+"use strict";
 
-const tape = require('tape');
+const tape = require("tape");
 
-const fixtures = require('./fixtures');
+const fixtures = require("./fixtures");
 
 const cli = fixtures.cli;
 const cmd = fixtures.cmd;
 
-tape('evtag interop', (t) => {
+tape("evtag interop", (t) => {
   const repos = [
-    'git://github.com/cgwalters/git-evtag.git',
-    'git://github.com/ostreedev/ostree.git',
-    'git://github.com/GNOME/gnome-terminal.git',
-    'https://gitlab.com/fidencio/libosinfo.git'
+    "git://github.com/cgwalters/git-evtag.git",
+    "git://github.com/ostreedev/ostree.git",
+    "git://github.com/GNOME/gnome-terminal.git",
+    "https://gitlab.com/fidencio/libosinfo.git",
   ];
 
   repos.forEach((url) => {
@@ -23,8 +23,10 @@ tape('evtag interop', (t) => {
     // Verify tags
     const node = process.execPath;
     tags.forEach((tag) => {
-      t.doesNotThrow(() => cmd(node, [ cli, '--insecure', '-v', tag ]),
-                     `tag ${tag} of ${url} should validate`);
+      t.doesNotThrow(
+        () => cmd(node, [cli, "--insecure", "-v", tag]),
+        `tag ${tag} of ${url} should validate`
+      );
     });
 
     fixtures.destroy();
