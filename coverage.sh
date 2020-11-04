d1403717d636c3da7fa30ffbf7b24cf589fa5265
sam <sam@freighttrust.com>
2020-11-04 02:44:51 -0800

    refactor

--- coverage.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-export CODECOV_TOKEN="23544ffd-233e-4f44-9482-2d948de67155"
-./node_modules/.bin/nyc tape test/api-test.js
-#./node_modules/.bin/nyc tape test/ev-tag-test.js
-#./node_modules/.bin/nyc tape test/scenario-test.js
-./node_modules/.bin/nyc report --reporter=text-lcov | coveralls
